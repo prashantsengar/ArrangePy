0ba6a1cc7d9e8316b98e3508466bd1ca46a6eba3
Ashish013 <56002521+Ashish013@users.noreply.github.com>
2019-12-04 09:46:59 +0530

    Adding Functionality Pc Health and Info
    1> Also added the extension for IOS files.
    2> Added the extensions for executables, docs, IOS for Strong arrange.

--- arrange.py
+++ arrange.py
@@ -4,73 +4,112 @@
 
 import os
 import shutil
+import sys
+from pchealth import system_health as sysinfo
+#Imports class system_health as sysinfo from the module pchealth
+
 
 def makeFolders(f):
     if f in os.listdir():
         return
     os.mkdir(f'.\\{f}')
 
-makeFolders('pPDF')
-makeFolders('Pimages')
-makeFolders('Pvideos')
-makeFolders('Paudios')
-makeFolders('Pprograms')
-makeFolders('Pdocs')
+makeFolders('PDF')
+makeFolders('Images')
+makeFolders('Videos')
+makeFolders('Audios')
+makeFolders('Programs')
+makeFolders('Docs')
 
 def strong_arrange():
         for foldername, subfolders, filenames in os.walk(folder):
             for file in filenames:
-                if file.lower().endswith('.pPDF'):
+                if file.lower().endswith('.pPDF') or file.lower().endswith('.PDF'):
                         print(file)
-                        shutil.move(file,'.\\pPDF')
+                        shutil.move(file,'.\\PDF')
                 elif file.lower().endswith('png') or file.lower().endswith('jpg') or file.lower().endswith('jpeg') or file.lower().endswith('gif'):
                     print(file)
-                    shutil.move(file,'.\\Pimages')
+                    shutil.move(file,'.\\Images')
                 elif file.lower().endswith('.mp4') or file.lower().endswith('.mkv') or file.lower().endswith('.avi') or file.lower().endswith('.3gp'):
                     print(file)
-                    shutil.move(file,'.\\Pvideos')
+                    shutil.move(file,'.\\Videos')
                 elif file.lower().endswith('.mp3') or file.lower().endswith('.wav'):
                     print(file)
-                    shutil.move(file,'.\\Paudios')
+                    shutil.move(file,'.\\Audios')
+                elif file.lower().endswith('.exe'):
+                    print(file)
+                    shutil.move(file,'.\\Programs')
+                elif file.lower().endswith('.ios'):
+                    print(file)
+                    shutil.move(file,'.\\Ios Files')
+                elif (file.lower().endswith('.docx') or file.lower().endswith('.doc') or
+                file.lower().endswith('.xlsx') or file.lower().endswith('.xls') or
+                file.lower().endswith('.txt') or file.lower().endswith('.csv') or
+                file.lower().endswith('.pptx') or file.lower().endswith('.ppt')):
+                    print(file)
+                    shutil.move(file,'.\\Docs')
          
 
 def arrange():
     for file in os.listdir(folder):
         if file.lower().endswith('.pdf') :
             print(file)
-            shutil.move(file,'.\\pPDF')
+            shutil.move(file,'.\\PDF')
         elif file.lower().endswith('png') or file.lower().endswith('jpg') or file.lower().endswith('jpeg') or file.lower().endswith('gif'):
             print(file)
-            shutil.move(file,'.\\Pimages')
+            shutil.move(file,'.\\Images')
         elif file.lower().endswith('.mp4') or file.lower().endswith('.mkv') or file.lower().endswith('.avi') or file.lower().endswith('.3gp'):
             print(file)
-            shutil.move(file,'.\\Pvideos')
+            shutil.move(file,'.\\Videos')
         elif file.lower().endswith('.mp3') or file.lower().endswith('.wav'):
             print(file)
-            shutil.move(file,'.\\Paudios')
+            shutil.move(file,'.\\Audios')
         elif file.lower().endswith('.exe'):
             print(file)
-            shutil.move(file,'.\\Pprograms')
+            shutil.move(file,'.\\Programs')
+        elif file.lower().endswith('.ios'):
+            print(file)
+            shutil.move(file,'.\\Ios Files')
         elif (file.lower().endswith('.docx') or file.lower().endswith('.doc') or
         file.lower().endswith('.xlsx') or file.lower().endswith('.xls') or
         file.lower().endswith('.txt') or file.lower().endswith('.csv') or
         file.lower().endswith('.pptx') or file.lower().endswith('.ppt')):
             print(file)
-            shutil.move(file,'.\\Pdocs')
+            shutil.move(file,'.\\Docs')
  
 
+def pcinfo():
+    if sys.platform == "win32":
+        sysinfo.userinfo()
+        sysinfo.memory_info()
+        sysinfo.virt_memory()
+        sysinfo.batteryinfo()
+    else:
+        try:
+            sysinfo.userinfo()
+            sysinfo.memory_info()
+            sysinfo.virt_memory()
+            sysinfo.batteryinfo()
+            sysinfo.system_temp()
+            sysinfo.fan_speed()
+        except:
+            print("It can't be determined for the present OS")
+        
+ 
 if __name__ == '__main__':
 
     print("Arrange files")
     folder=os.getcwd()
     print(folder)
 
-    choice = int(input("Press 1 for Weak arrange\nPress 2 for Strong arrange\n0 to exit"))
+    choice = int(input("Press 1 for Weak arrange\nPress 2 for Strong arrange\nPress 3 for Pc Health and Info\nPress 0 to exit the program"))
 
     if choice==1:
         arrange()
-    if choice==2:
+    elif choice==2:
         strong_arrange()
-    if choice==0:
+    elif choice==3:
+        pcinfo()
+    elif choice==0:
         exit(0)
     
