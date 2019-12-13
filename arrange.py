fe8707f567b447a5430f7cc7189ad3a6dd669c34
Ravishankar <chavare.ravi123@gmail.com>
2019-12-14 02:04:45 +0530

    Added Some Exception Handeler,Added Cross Platform Comapatible code,Improved printing message

--- arrange.py
+++ arrange.py
@@ -5,16 +5,16 @@
 import os
 import shutil
 
-FOLDER_TYPES={'pPDF':['pdf'],
+FOLDER_TYPES = {'pPDF':['pdf'],
               'Pimages':['png','jpeg','jpg','gif'],
               'Pvideos':['mp4','mkv','avi','3gp'],
               'Paudios':['mp3','wav'],
               'Pprograms':['exe'],
               'Pdocs':['xlsx','doc','xlsx','pptx','csv','txt','ppt']
               }
-RESULT_DIR='output'
+RESULT_DIR = 'output'
 
-def identyType(ext):
+def identifyType(ext):
     '''
     Accept extenssion Example .pdf .mp4 and
     return a category type from FOLDER_TYPES Dictionary
@@ -32,10 +32,14 @@ def makeFolders(lst):
     Accept A List of Folder name and
     create that category name folder in RESULT_DIR
     '''
+    
+    if os.path.exists(RESULT_DIR) is False:
+        os.mkdir(RESULT_DIR)
+                      
     for name in lst:
         if name in os.listdir(RESULT_DIR):
             return
-        os.mkdir(f'{RESULT_DIR}\\{name}')
+        os.mkdir(os.path.join(RESULT_DIR,name))
 
 
 def moveFiles(src,dst):
@@ -43,13 +47,12 @@ def moveFiles(src,dst):
     Accept source and destination and move files .
     Return True if File is Copied other wise False
     '''
-    res=True
+    res = True
     try:
         pass
-        #print(src,dst)
-        #shutil.move(src,os.path.join(RESULT_DIR,dst))
+        shutil.move(src,os.path.join(RESULT_DIR,dst))
     except:
-        res=False
+        res = False
     return res
 
 #Create Output and category folder if not Exists
@@ -61,9 +64,9 @@ def startProcess(folder,file):
     Return a Tuple(TRUE|FALSE,TYPE_OF_FILE)
     
     '''
-    types=os.path.splitext(file)[1].lower()
-    src=os.path.join(folder,file)
-    dst=identyType(types)
+    types = os.path.splitext(file)[1].lower()
+    src = os.path.join(folder,file)
+    dst = identifyType(types)
     if dst is not None:
         return moveFiles(src,dst),dst
     return False,'Others(Not_moved)'
@@ -73,19 +76,19 @@ def strong_arrange():
     for foldername, subfolders, filenames in os.walk(folder):
         for file in filenames:
             if os.path.isfile(os.path.join(folder,file)):
-               status,types=startProcess(folder,file)
+               status,types = startProcess(folder,file)
                if types in TOTAL_COUNT:
-                   TOTAL_COUNT[types]=TOTAL_COUNT[types]+1
+                   TOTAL_COUNT[types] = TOTAL_COUNT[types]+1
                else:
-                   TOTAL_COUNT[types]=1
+                   TOTAL_COUNT[types] = 1
     return TOTAL_COUNT
 def arrange():
-    TOTAL_COUNT={}
+    TOTAL_COUNT = {}
     for file in os.listdir(folder):
         if os.path.isfile(os.path.join(folder,file)):
-            status,types=startProcess(folder,file)
+            status,types = startProcess(folder,file)
             if types in TOTAL_COUNT:
-                TOTAL_COUNT[types]=TOTAL_COUNT[types]+1
+                TOTAL_COUNT[types] = TOTAL_COUNT[types]+1
             else:
                 TOTAL_COUNT[types]=1
     return TOTAL_COUNT
@@ -98,15 +101,19 @@ if __name__ == '__main__':
 
     choice = int(input("Press 1 for Weak arrange\nPress 2 for Strong arrange\n0 to exit\noption:"))
 
-    if choice==1:
-        res=arrange()
-    if choice==2:
-        res=strong_arrange()
-    if choice==0:
+    if choice == 1:
+        res = arrange()
+    if choice == 2:
+        res = strong_arrange()
+    if choice == 0:
         exit(0)
 
     #Final Result
-    print('{0:*^30s}'.format('Result'))
+    message = "Result"
+    others="Others(Not_moved)"
+    print(f'{message:*^30s}')
     for key,value in res.items():
-        print(f'{key}:{value}')
-    
+        if key == others:
+            continue
+        print(f'{value} file moved into Category {os.path.join(RESULT_DIR,key)}')
+    print(f'{res[others]} file Not moved')    
