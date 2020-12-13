67507c18dec84e842b759a7073cbc01df8a449fb
Samyak <skcy2001@gmail.com>
2020-12-13 14:35:31 +0530

    makeFolders seperated

---
+++ make_folders.py
@@ -0,0 +1,13 @@
+import os
+
+def makeFolders(root,lst):
+    '''
+    Accept A List of Folder name and
+    create that category name folder in RESULT_DIR
+    '''
+    if os.path.exists(root) is False:
+        os.mkdir(root)
+                      
+    for name in lst:
+        if name not in os.listdir(root):
+            os.mkdir(os.path.join(root,name))
