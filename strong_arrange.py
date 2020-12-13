030f1f3887adfce31c677ff21b7f8e3673d99894
Samyak <skcy2001@gmail.com>
2020-12-13 13:35:09 +0530

    seperated strong arrange

---
+++ strong_arrange.py
@@ -0,0 +1,29 @@
+
+import os
+import shutil
+from move_files import *
+import sys
+from configure import configur
+import re
+
+RESULT_DIR = 'CleanedPy'
+try:
+    RESULT_DIR = os.path.join(sys.argv[1],RESULT_DIR)
+except:
+    pass
+
+FOLDER_TYPES = configur()
+
+def strong_arrange(root,destination,index):
+    TOTAL_COUNT={}
+    for foldername, subfolders, filenames in os.walk(root):
+        for file in filenames:
+            if os.path.isfile(os.path.join(foldername,file)):
+               status,types = startProcess(foldername,file,index,destination)
+               if types in TOTAL_COUNT:
+                   TOTAL_COUNT[types] = TOTAL_COUNT[types]+1
+               else:
+                   TOTAL_COUNT[types] = 1
+    return TOTAL_COUNT
+
+print(strong_arrange(sys.argv[1],RESULT_DIR,FOLDER_TYPES))
