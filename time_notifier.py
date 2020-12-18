56d71577b8db9f27ede81dc3e5ce8a6e887b082e
encrypted-jpg <vallakatlaaditya@gmail.com>
2020-12-18 14:47:54 +0530

    Added GUI for Time-Notifier file

--- time_notifier.py
+++ time_notifier.py
@@ -1,9 +1,13 @@
 import time
+import tkinter
+import tkinter.font
+from pynotifier import Notification
+from random import choice
+from threading import Timer
 import datetime
-from win10toast_persist import ToastNotifier
 
 
-def it_is_time():
+def it_is_time_for_notify():
     """
 
     This Function returns the Current Time and Date with the Standard Format in a String.
@@ -18,33 +22,123 @@ def it_is_time():
     if h > 12:
         h = h - 12
         meridian = "PM"
-    notifies = f"Time: {h}:{m}:{s} {meridian}\nDate: {d}\n"
+    notifies = f"Time: {h}:{m}:{s} {meridian} -+- Date: {d}\n"
     return notifies
+
+
+def notify(hours):
+    """
+
+    This Function notifies when it's an hour, two hours or more.
+
+    """
+    present = it_is_time_for_notify()
+    if hours == 1:
+        Notification("It's an Hour!!", present).send()
+    else:
+        ptr = f"It's {hours} Hours"
+        Notification(ptr, present).send()
+    Timer(3600, function=lambda: notify(hours + 1)).start()
+
+
+def snooze(window):
+    """
+
+    This Function snoozes for 5min and then again shows the 'Break' Window.
+
+    """
+    window.destroy()
+    time.sleep(300)
+    App()
+
+
+def go_next(window):
+    """
+
+    This Function Destroys the 'Break' Window.
+
+    """
+    window.destroy()
+
+
+class App(tkinter.Tk):
+    """
+
+    This Class is inherited from tkinter.Tk, creates and configures the window whilst updating the clock on display.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        tkinter.Tk.__init__(self, *args, **kwargs)
+        self.title("20-20-20")
+        self.lab = tkinter.Label(
+            self,
+            text="Every 20 minutes, look at something 20 feet away for 20 seconds.".center(
+                80
+            ),
+            font=tkinter.font.Font(size=15),
+        )
+        self.geometry("700x220")
+        file = open("quotes.txt", "r")
+        lst = file.read().splitlines()
+        quote = choice(lst)
+        self.quote_lab = tkinter.Label(
+            self, text=quote.center(80), font=tkinter.font.Font(size=10)
+        )
+        self.present_lab = tkinter.Label(
+            self, text=it_is_time_for_notify(), font=tkinter.font.Font(size=15)
+        )
+        self.lab.grid(column=0, row=0)
+        self.quote_lab.grid(column=0, row=4)
+        self.present_lab.grid(column=0, row=1)
+        selfWidth = self.winfo_reqwidth()
+        selfHeight = self.winfo_reqheight()
+        positionRight = int(self.winfo_screenwidth() / 2 - selfWidth)
+        positionDown = int(self.winfo_screenheight() / 2 - selfHeight)
+        self.geometry("+{}+{}".format(positionRight, positionDown))
+        self.attributes("-topmost", True)
+        bt1 = tkinter.Button(
+            self,
+            text="Snooze for 5 min",
+            padx=1,
+            pady=5,
+            command=lambda: snooze(self),
+            font=tkinter.font.Font(size=15),
+        )
+        bt1.grid(column=0, row=2)
+        bt2 = tkinter.Button(
+            self,
+            text="Go for Next Break",
+            padx=1,
+            pady=5,
+            command=lambda: go_next(self),
+            font=tkinter.font.Font(size=15),
+        )
+        bt2.grid(column=0, row=3)
+        self.update_clock()
+        self.mainloop()
+
+    def update_clock(self):
+        """
+
+        This function updates the clock displayed on the window.
+
+        """
+        self.present_lab.configure(text=it_is_time_for_notify())
+        self.after(1000, self.update_clock)
 
 
 def main():
     """
 
-    This Function Notifies every half-an-hour.
-    If it's 0.5, 1.5, 2.5, etc hr it notifies the Current Date and Time.
-    If it's 1, 2, 3.. hr it notifies how many hrs that's been passed. Every count starts from the beginning of
-    execution of the program.
+    This function initializes the Hour Timer and Break Timer.
 
     """
-    toaster = ToastNotifier()
-    n = 0
-    notify = "Timer Started"
-    toaster.show_toast(notify, duration=5)
-    while n < 12:
-        time.sleep(1800)
-        toaster.show_toast(it_is_time(), duration=5)
-        time.sleep(1800)
-        n += 1
-        if n == 1:
-            nof = "It's one hour"
-        else:
-            nof = f"It's {n} hours"
-        toaster.show_toast(nof, it_is_time(), duration=5)
+    Notification("Started Timer...", " ").send()
+    Timer(3600, function=lambda: notify(1)).start()
+    while True:
+        time.sleep(1200)
+        App()
 
 
 main()
