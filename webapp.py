b3e51e0495c5e14af8e689c3f078f7102b8a71c7
devsTudu <65746343+devsTudu@users.noreply.github.com>
2020-12-22 18:36:00 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template, request, redirect, url_for
-from main import *
+from flask import Flask, render_template, request, redirect, url_for
+import lib.arrange
+import lib.utils
 import webbrowser
 import time
 import os
@@ -7,14 +8,16 @@ import os
 app = Flask(__name__)
 
 
-
 def startwork():
     ''' This will create folders to keep files according to their file types.  '''
-    global destination
+    nonlocal destination
     destination = os.path.join(TARGET_FOLDER, RESULT_DIR)
     lib.utils.makeFolders(destination, FOLDER_TYPES.keys())
 
+    
 @app.route('/dashboard')
+
+
 def dashboard():
     '''The page where the number of items present in the input location is given'''
     directory = TARGET_FOLDER
@@ -24,48 +27,63 @@ def dashboard():
     return render_template("index.html", nfol=folder_count, nfile=file_count, total=total_items, workdir=directory)
 
 
-@app.route('/input',methods = ['POST', 'GET'])
+@app.route('/input', methods=['POST', 'GET'])
+
+
 def inputuser():
     '''Take the input of the address from HTML methods'''
     if request.method == 'POST':
-      val = request.form['newaddress']
+        val = request.form['newaddress']
     else:
-      val = request.args.get('newaddress')
+        val = request.args.get('newaddress')
     if os.path.isdir(val):
-        global TARGET_FOLDER
+        nonlocal TARGET_FOLDER
         TARGET_FOLDER = str(val)
         return redirect(url_for('dashboard'))
+    
     else:
         return render_template("error404.html")
+    
 
 @app.route('/changelocation')
+
+
 def changingPage():
     '''Initiate and asks for the address as text input, where the app need to do the cleaning work'''
     return render_template('change.html')
 
 
 @app.route('/standardscan')
+
+
 def stdScan():
-    '''Initiate the arrange function from the main module, and redirect to report page'''
+    '''Initiate the arrange function, and redirect to report page after the process is complete'''
     startwork()
     report = lib.arrange.weak_arrange(TARGET_FOLDER, destination, FOLDER_TYPES)              # taking reports of the file transfer
     return render_template('completed.html', res=report)
 
+
 @app.route('/deepscan')
+
+
 def deepScan():
-    '''Initiate the strong arrange function from main module, and redirect to the report page after the transfer is complete'''
+    '''Initiate the strong arrange function, and redirect to the report page after the transfer is complete'''
     startwork()
     report = lib.arrange.strong_arrange(TARGET_FOLDER, destination, FOLDER_TYPES)
     return render_template('completed.html', res=report)
 
+
 @app.route('/closetheapp')
+
+
 def close():
     '''Turn the web server OFF '''
     shutdown_server()
     return 'Server shutting down..'
-    
+
+
 if __name__ == '__main__':
     print("Running the program..")
     time.sleep(0.5)                                                  # Delay the run, to give the feeling of loading the app
-    webbrowser.open('http://127.0.0.1:5000/changelocation')           # opened the browser before running app, cause
-    app.run(debug=True)                                               # after running app, browser couldnot be automatically loaded
+    webbrowser.open('http://127.0.0.1:5000/changelocation')          #open the webpage automatically
+    app.run(debug=True)                                              
